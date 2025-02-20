a40ab393b6954df3e494caa817b48b2ccc9458d0
crediboost-0 <zeshanka04@gmail.com>
2025-02-20 15:16:06 +0000

    config.py and .env

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://crediboostdb_user:Ht4pT3m1t7BN5ACHHTalZ92Jm8WlhdEx@dpg-curihnbv2p9s73aj56f0-a/crediboostdb")  # Uses environment variable
+    SQLALCHEMY_DATABASE_URI = os.getenv(postgresql://crediboostdb_user:Ht4pT3m1t7BN5ACHHTalZ92Jm8WlhdEx@dpg-curihnbv2p9s73aj56f0-a/crediboostdb
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
