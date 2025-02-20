4604f2407c138c1d59d8f980c85480da315f6dd3
crediboost-0 <zeshanka04@gmail.com>
2025-02-20 13:01:27 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://postgres:[Zesh1604!]@db.bzaztdjdgpnpzocjvxnj.supabase.co:5432/postgres")  # Uses environment variable
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://postgres.bzaztdjdgpnpzocjvxnj:[YOUR-PASSWORD]@aws-0-eu-central-1.pooler.supabase.com:5432/postgres")  # Uses environment variable
     SQLALCHEMY_TRACK_MODIFICATIONS = False
